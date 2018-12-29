e9a5cd832f1ae0d1b04cdce19941a899cbbaea49
开水专烫鸡 <879574764@qq.com>
2018-12-29 22:40:11 +0800

    Add files via upload

---
+++ 2/server.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    int pid,err;
+    time_t timep;
+    struct tm *p;
+
+    char buf[1024];
+
+    int serv_sock,cli_sock;
+
+    struct sockaddr_in serv_addr;
+    struct sockaddr_in cli_addr;
+
+    /* init server socket */
+    serv_sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    bzero(&serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(8080);
+
+    err = bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+    if(err < 0){
+        printf("bind error\n");
+        return -1;
+    }
+
+    err = listen(serv_sock, 10);
+    if(err < 0){
+        printf("listen error\n");
+        return -1;
+    }
+
+    /* accept client request */
+    for(;;)	{
+        socklen_t addrlen = sizeof(struct sockaddr);
+
+        cli_sock = accept(serv_sock, (struct sockaddr*)&cli_addr, &addrlen);
+
+        if(cli_sock < 0)
+            continue;
+
+        pid = fork();
+        if(pid != 0){
+            // in child process
+            time(&timep);
+            p = gmtime(&timep);
+
+            strcpy(buf,"%d",p->tm_sec);
+
+            write(cli_sock,buf,sizeof(buf));
+            close(cli_sock);
+            return 0;
+        }
+    }
+}
